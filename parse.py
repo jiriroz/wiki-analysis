e7f3e0e0333686a061d3dabb8733949377705a0a
jirkaroz <jiri.roznovjak@gmail.com>
2015-10-10 23:33:22 -0400

    minor changes

--- parse.py
+++ parse.py
@@ -1,13 +1,24 @@
 import xml.etree.ElementTree as etree
+import time
 
-fName = "wikiDB"
-
+"""
+Counts the number of articles.
+"""
 count = 0
+fName = "wikiDBxml"
+startT = time.time()
+t = startT
 
-#print first 100 titles
 for event, elem in etree.iterparse(fName):
     if count > 100:
         break
     if elem.tag[-5:] == "title":
         count += 1
-        print elem.text.encode('utf-8')
+        #print title (need Unicode)
+        #print elem.text.encode('utf-8')
+    if time.time() - t > 60:
+        print str((time.time() - startT) // 60) + " min"
+        print "articles: " + count
+        t = time.time()
+
+print "Number of articles: " + count
